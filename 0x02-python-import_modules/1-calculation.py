f343df721d195cc3f11be76b87a268acdf13e592
Gold Israel <techiegold101@gmail.com>
2023-03-09 12:05:34 -0800

    Added back the line 3

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 
-from calculator_1 import mul, add, sub, div
+    from calculator_1 import mul, add, sub, div
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-addition = add(a, b)
-multiply = mul(a, b)
-subtract = sub(a, b)
-divide = div(a, b)
+    addition = add(a, b)
+    multiply = mul(a, b)
+    subtract = sub(a, b)
+    divide = div(a, b)
 
-print("{} + {} = {}".format(a, b, addition))
-print("{} - {} = {}".format(a, b, subtract))
-print("{} * {} = {}".format(a, b, multiply))
-print("{} / {} = {}".format(a, b, divide))
+    print("{} + {} = {}".format(a, b, addition))
+    print("{} - {} = {}".format(a, b, subtract))
+    print("{} * {} = {}".format(a, b, multiply))
+    print("{} / {} = {}".format(a, b, divide))
