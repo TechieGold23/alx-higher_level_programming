1486b4e122a2ea393a0283481b98fe7b9b36aa97
Gold Israel <techiegold101@gmail.com>
2023-03-09 06:46:09 -0800

    Added file

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from calculator_1 import mul, add, sub, div
+
+a = 10
+b = 5
+
+addition = add(a, b)
+multiply = mul(a, b)
+subtract = sub(a, b)
+divide = div(a, b)
+
+print(f"{a} + {b} = {addition}")
+print(f"{a} - {b} = {subtract}")
+print(f"{a} * {b} = {multiply}")
+print(f"{a} / {b} = {divide}")
