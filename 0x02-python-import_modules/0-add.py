1ddbd6a9bbc0a4ea2eac95ee76296906be61ec75
Gold Israel <techiegold101@gmail.com>
2023-03-09 12:09:29 -0800

    Added Name-Main idiom

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
 
-a = 1
-b = 2
+    from add_0 import add
 
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+    a = 1
+    b = 2
+
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
