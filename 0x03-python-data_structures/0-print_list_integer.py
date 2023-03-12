e619ff693b9d88c3d448c7d5263adbe523e5f2dd
Gold Israel <techiegold101@gmail.com>
2023-03-12 10:42:32 -0700

    this file contains a code that prints all integer of a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A function that prints all integers of a list
+    by: Gold Israel
+"""
+
+
+def print_list_integer(my_list=[]):
+
+    for i in my_list:
+        print("{}".format(i))
