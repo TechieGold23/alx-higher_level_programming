4fe2158465b0b5ad2eabe0219ccc40ed80e5c4e2
Gold Israel <techiegold101@gmail.com>
2023-03-13 05:01:11 -0700

    This code replaces an element of a list at any POS

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+""" A function that replaces an element of a list at a specific position
+   By; Gold Israel
+
+   Args:
+       my_list: A list of elements
+       idx: An integer represntig the index of the element to be replaced
+       element: The new element to replace the old element with.
+
+
+   Returns:
+       The new list.
+ """
+
+
+def replace_in_list(my_list, idx, element):
+
+    my_list[idx] = element
+
+    if idx < 0:
+        return None
+
+    elif idx >= len(my_list):
+        return None
+
+    else:
+        return my_list
