3330d843c99f609f344cd8dcfe02a90d2ff22a60
Gold Israel <techiegold101@gmail.com>
2023-03-13 13:37:02 -0700

    Assigned my_list to lst

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,7 +5,7 @@
 
    Args:
        my_list: A list of elements
-       idx: An integer represntig the index of the element to be replaced
+       idx: An integer representig the index of the element to be replaced
        element: The new element to replace the old element with.
 
 
@@ -16,13 +16,14 @@
 
 def replace_in_list(my_list, idx, element):
 
+    lst = my_list
     my_list[idx] = element
 
     if idx < 0:
-        return None
+        return lst
 
     elif idx >= len(my_list):
-        return None
+        return lst
 
     else:
         return my_list
