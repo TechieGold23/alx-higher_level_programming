fb7c4dd162b8799b445eee4f6e5df766948df6ae
Gold Israel <techiegold101@gmail.com>
2023-03-13 05:31:46 -0700

    This code prints a list of int in rev order

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""A function that prints all integers of a list in reverse order
+   By: Gold Israel
+
+
+   Args:
+       my_list: A list of integers
+
+
+    Return:
+         Returns reversed list.
+"""
+
+
+def print_reversed_list_integer(my_list=[]):
+    rev_list = my_list[::-1]
+    for i in rev_list:
+        print("{:d}".format(i))
