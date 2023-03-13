8fb1ec15f99dec3a466498eb2c640049d89f2c13
Gold Israel <techiegold101@gmail.com>
2023-03-13 14:39:12 -0700

    Modified the code for readability

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -15,14 +15,13 @@
 
 
 def new_in_list(my_list, idx, element):
-    new_list = list(my_list)
-    new_list[idx] = element
-
+    new_list = my_list.copy()
     if idx < 0:
-        return my_list
+        return my_list.copy()
 
-    elif idx >= len(my_list):
-        return my_list
+    elif idx > len(new_list) - 1:
+        return my_list.copy()
 
     else:
+        new_list[idx] = element
         return new_list
