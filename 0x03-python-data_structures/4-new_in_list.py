32477e44aa69b7825ffbf84ffee4c7d5ab06f0de
Gold Israel <techiegold101@gmail.com>
2023-03-13 07:18:30 -0700

    Created new_list and assigned my_list to it

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -15,14 +15,14 @@
 
 
 def new_in_list(my_list, idx, element):
-    lst = my_list
-    my_list[idx] = element
+    new_list = list(my_list)
+    new_list[idx] = element
 
     if idx < 0:
-        return lst
+        return my_list
 
     elif idx >= len(my_list):
-        return lst
+        return my_list
 
     else:
-        return my_list
+        return new_list
